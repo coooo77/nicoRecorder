4e5d90f3c520094548472328b91b177366a26f08
TL Chang <coooo77@hotmail.com>
2020-10-31 12:34:21 +0800

    [修正]系統提示錯誤問題

--- config/announce.js
+++ config/announce.js
@@ -1,38 +1,38 @@
 module.exports = {
   init: {
-    recorder:{
-      isNotExist:'Directory recorder is not exist',
+    recorder: {
+      isNotExist: 'Directory recorder is not exist',
       startToCreateDirectory: 'Start to create recorder directory'
     },
-    model:{
+    model: {
       isNotExist: 'Directory model is not exist',
       startToCreateDirectory: 'Start to create model directory'
     },
-    streamRecords:{
+    streamRecords: {
       isNotExist: 'StreamRecords.json is not exist',
       startToCreate: 'Start to create streamRecords.json'
     },
-    usersData:{
+    usersData: {
       isNotExist: 'UsersData.json is not exist',
       startToCreate: 'Start to create usersData.json'
     },
-    initiationIsFinished:'Initiation finished'
+    initiationIsFinished: 'Initiation finished'
   },
-  app:{
-    startToLogin:'User needs to login, start to login...',
-    startToFetchRecords:'Start to fetch stream records...',
-    findNewRecord:'Find a new record, save it to model and record the stream',
-    startToRecord:{
-      pre:'Start to record user ',
-      post:', url of stream is '
+  app: {
+    startToLogin: 'User needs to login, start to login...',
+    startToFetchRecords: 'Start to fetch stream records...',
+    findNewRecord: 'Find a new record, save it to model and record the stream',
+    startToRecord: {
+      pre: 'Start to record user ',
+      post: ', url of stream is '
     },
-    stopToRecord:{
-      pre:'User',
-      post:' record setting is disabled, stop to record'
+    stopToRecord: {
+      pre: 'User ',
+      post: ' record setting is disabled, stop to record'
     },
-    recordsStatus:{
+    recordsStatus: {
       isUpDated: 'Records updated',
-      isUnChanged:'Records unChanged'
+      isUnChanged: 'Records unChanged'
     }
   }
 }
--- nico.js
+++ nico.js
@@ -1,15 +1,15 @@
 const { setting, checkStreamInterval } = require('./config/config.js')
-const helper = require('./util/helper')
+const { timeAnnounce } = require('./util/helper')
 const nico = require('./app')
 const puppeteer = require('puppeteer-core');
 (async () => {
   console.log('[System]Start to monitor nico web site ...')
   let count = 1
-  helper.timeAnnounce(count)
+  timeAnnounce(count++)
   const browser = await puppeteer.launch(setting);
   await nico(browser)
   setInterval(async function () {
-    helper.timeAnnounce(count)
+    timeAnnounce(count++)
     await nico(browser)
   }, checkStreamInterval)
 })()
