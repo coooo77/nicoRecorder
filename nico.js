2db641ad6cc43578e72e3298fe5f1a257f017222
TL Chang <coooo77@hotmail.com>
2020-11-16 20:40:05 +0800

    [優化] 減少快取消耗

--- nico.js
+++ nico.js
@@ -3,14 +3,33 @@ const { nico } = require('./config/announce')
 const { timeAnnounce, announcer } = require('./util/helper')
 const app = require('./app')
 const puppeteer = require('puppeteer-core');
+
+function runningApp(count, browser) {
+  return new Promise((resolve, reject) => {
+    try {
+      let test = setInterval(async function () {
+        announcer(nico.timeAnnounce(count++), true)
+        await app(browser)
+        if (count === 1000) {
+          clearInterval(test)
+          test = null
+          resolve(count)
+        }
+      }, checkStreamInterval);
+    } catch (error) {
+      console.log(error)
+      reject()
+    }
+  })
+}
+
 (async () => {
   announcer(nico.startToMonitor)
   let count = 1
-  announcer(nico.timeAnnounce(count++),true)
+  announcer(nico.timeAnnounce(count++), true)
   const browser = await puppeteer.launch(setting);
   await app(browser)
-  setInterval(async function () {
-    announcer(nico.timeAnnounce(count++), true)
-    await app(browser)
-  }, checkStreamInterval)
+  while (true) {
+    count = await runningApp(count, browser)
+  }
 })()
